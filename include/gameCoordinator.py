a698d73f6f869a5a3216e2c84079117d8d7403c7
Lucas Roberto Raineri Oliveira <lucasrobertoliveira@gmail.com>
2021-12-06 10:22:12 -0300

    It kinda draws the puzzle now

--- include/gameCoordinator.py
+++ include/gameCoordinator.py
@@ -16,22 +16,20 @@ class GameCoordinator():
     def __init__(self, solution: PicrossPuzzle) -> None:
         self.solution = solution.getSolution()
         self.workingArea = solution.getWorkingArea()
+        self.puzzleArea = solution.getPuzzleArea()
         self.localMatrix = []
         # Build a similarly dimensioned matrix
-        xDimension = 0
-        yDimension = 0
+        self.xDimension = 0
+        self.yDimension = 0
         for line in solution.getSolution():
-            yDimension += 1
+            self.yDimension += 1
+            # TODO fix this so it accepts non square matrices
+            self.xDimension += 1
             localLine = []
             for term in line:
                 localLine.append(0)
-                xDimension += 1
             self.localMatrix.append(localLine)
         # Grid vertical space in px following (xMax - xMin) / termQty
-        self.verticalSize = int(
-            (self.workingArea[2] - self.workingArea[0]) / xDimension)
-        self.horizontalSize = int(
-            (self.workingArea[3] - self.workingArea[1]) / yDimension)
 
     def checkMatrixParity(self):
         for solutionLine, localLine in self.solution, self.localMatrix:
@@ -40,7 +38,39 @@ class GameCoordinator():
                     return False
         return True
 
-    def draw(self,windowRenderer):
-        pygame.gfxdraw.line(windowRenderer.getWindow(
-        ), self.workingArea[0], self.workingArea[1], self.workingArea[2], self.workingArea[3], COLOR_FOREGROUND)
+    def draw(self, windowRenderer):
+        windowRenderer.getWindow().fill(COLOR_BACKGROUND)
+        localX = self.puzzleArea[0]
+        localY = self.puzzleArea[1]
+        localXdimension = 0
+        localYdimension = 0
+        verticalSize =  int(
+            (self.puzzleArea[3] - self.puzzleArea[1]) / self.yDimension)
+        horizontalSize =int(
+            (self.puzzleArea[2] - self.puzzleArea[0]) / self.xDimension)
+        finalLocalX = localX + (self.xDimension*horizontalSize)
+        finalLocalY = localY + (self.yDimension*verticalSize)
+
+        while(True):
+            if(localXdimension >= self.xDimension and localYdimension >= self.yDimension):
+                # pygame.gfxdraw.line(windowRenderer.getWindow(
+                # ), localX, self.puzzleArea[0], localX, self.puzzleArea[3], COLOR_FOREGROUND)
+                pygame.gfxdraw.line(windowRenderer.getWindow(
+                ), localX, self.puzzleArea[1], localX, finalLocalY, COLOR_FOREGROUND)
+                break
+            else:
+                if(localXdimension >= self.xDimension):
+                    localX = self.puzzleArea[0]
+                    localXdimension = 0
+                    localY += verticalSize
+                    localYdimension += 1
+                # FIXME o print ta errado
+                pygame.gfxdraw.line(windowRenderer.getWindow(
+                ), localX, localY, finalLocalX, localY, COLOR_FOREGROUND)
+                pygame.gfxdraw.line(windowRenderer.getWindow(
+                ), localX, localY, localX, finalLocalY, COLOR_FOREGROUND)
+                localX += horizontalSize
+                localXdimension += 1
+                # localY += self.verticalSize
+
         pass
--- include/puzzle.py
+++ include/puzzle.py
@@ -11,19 +11,28 @@ class PicrossPuzzle:
             # NOTE these are coordinates (xMin,yMin,xMax,yMax)
             # NOTE for 640x480 (4:3) there will be no bounding box
             localResolution = GameSettings().getResolution()
+            # TODO test this
             if(localResolution[0] / localResolution[1] == 4/3):
                 self.workingArea = (0, 0, 0, 0)
+                self.puzzleArea = (
+                    int(localResolution[0] * 1.4), int(localResolution[1] * 1.4), int(localResolution[0]), int(localResolution[1]))
             else:
                 self.workingArea = (int(localResolution[0]*0.15), int(localResolution[1]*0.1), int(
                     localResolution[0]*0.85), int(localResolution[1]*0.9))
+                self.puzzleArea = (
+                    int(self.workingArea[0] * 1.4), int(self.workingArea[1] * 1.4), int(self.workingArea[2]), int(self.workingArea[3]))
         elif filename:
             self.solution = readMatrix(filename)
             localResolution = GameSettings().getResolution()
             if(int(localResolution[0]) / int(localResolution[1]) == 4/3):
                 self.workingArea = (0, 0, 0, 0)
+                self.puzzleArea = (
+                    int(localResolution[0] * 1.4), int(localResolution[1] * 1.4), int(localResolution[0]), int(localResolution[1]))
             else:
                 self.workingArea = (int(int(localResolution[0])*0.15), int(int(localResolution[1])*0.1), int(
                     int(localResolution[0])*0.85), int(int(localResolution[1])*0.9))
+                self.puzzleArea = (
+                    int(self.workingArea[0] * 1.4), int(self.workingArea[1] * 1.4), int(self.workingArea[2]), int(self.workingArea[3]))
 
     def getSolution(self):
         return self.solution
@@ -36,3 +45,6 @@ class PicrossPuzzle:
 
     def setWorkingArea(self, workingAreaTuple):
         self.workingArea = workingAreaTuple
+
+    def getPuzzleArea(self):
+        return self.puzzleArea
